ac78df7278fb7b10cb842e663fc0b28c05239931
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2020-06-01 13:24:10 +0530

    Invert Binary Tree

---
+++ Day1.java
@@ -0,0 +1,18 @@
+//Invert Binary Tree
+
+
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null)
+            return root;
+        
+         TreeNode left=  invertTree(root.left);
+         TreeNode right=   invertTree(root.right);
+        
+        root.left=right;
+        root.right=left;
+     return root;
+        
+    }
+}
