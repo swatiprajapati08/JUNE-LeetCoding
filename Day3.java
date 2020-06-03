cc0e0ff40e4f74dd1d795697cfdeb253b994b5bf
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2020-06-03 15:23:22 +0530

    Two City Scheduling
    There are 2N people a company is planning to interview. The cost of flying the i-th person to city A is costs[i][0], and the cost of flying the i-th person to city B is costs[i][1].
    
    Return the minimum cost to fly every person to a city such that exactly N people arrive in each city.

---
+++ Day3.java
@@ -0,0 +1,43 @@
+//Two City Scheduling
+
+
+
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+       Comparator co = new Comparator<int[]>() {
+            public int compare(int[] a, int[]b){
+                return Math.abs(b[0] - b[1]) - Math.abs(a[0] - a[1]);
+            }
+        };
+        int n  = costs.length/2;
+        int res = 0;
+        int countA = 0; int countB = 0;
+        Arrays.sort(costs, co);
+        
+       /* for(int i = 0; i < costs.length; i++){
+            for(int j = 0; j < costs[0].length; j++){
+                System.out.print(costs[i][j] + " ");
+            }
+            System.out.println();
+        }*/
+        
+        for(int i = 0; i < costs.length; i++){
+            if(countA < n && countB < n){
+                if(costs[i][0] < costs[i][1]){
+                    countA++; res += costs[i][0];
+                }
+                if(costs[i][0] >= costs[i][1]){
+                    countB++; res += costs[i][1];
+                }                
+            }
+            else if(countA < n){
+                countA++; res += costs[i][0];
+            }
+            else if(countB < n){
+                countB++; res += costs[i][1];
+            }
+        }
+        
+        return res;
+    }
+}
