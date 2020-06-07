f382395cbeb4f60c7f003263685c026ddd51cf47
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2020-06-07 22:12:07 +0530

    Coin Change 2  Solution

---
+++ Day7.java
@@ -0,0 +1,17 @@
+//Coin Change 2
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        
+     int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+}Coin Change 2
+
+Solution
