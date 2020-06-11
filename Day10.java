5cbf75177649c812c79cef3ba18e3f1ec84a3e5c
Swati Prajapati <42577922+swatiprajapati08@users.noreply.github.com>
2020-06-11 11:37:35 +0530

    Search Insert Position

---
+++ Day10.java
@@ -0,0 +1,15 @@
+//Search Insert Position
+
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+       
+        int i=0;
+        for( i=0;i<nums.length;i++)
+        {
+            if(nums[i]>=target)
+                return i;
+        }
+        return i;
+    }
+}
